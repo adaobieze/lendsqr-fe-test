1bab79f26d455932f254e08854fc459cde724fef
Adaobi Eze <adaobiezeonyedika19@gmail.com>
2024-06-25 09:24:39 +0100

    fix: types file updated

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -45,3 +45,65 @@ export interface User {
     dateJoined: string;
     status: string;
 };
+
+
+export interface UserDetails {
+    id: string;
+    userPhoto: string;
+    userTier: number;
+    personalInfo: UserPersonalInfo;
+    educationAndEmployment: UserEducationAndEmployment;
+    socials: UserSocials;
+    guarantors: UserGuarantor[];
+    bankDetails: UserBankDetails;
+}
+
+interface UserPersonalInfo {
+    userName: string;
+    userRegNo: string;
+    phoneNumber: string;
+    email: string;
+    bvn: string;
+    gender: string;
+    maritalStatus: string;
+    children: string;
+    typeOfResidence: string;
+}
+
+interface UserEducationAndEmployment {
+    educationLevel: string;
+    employmentStatus: string;
+    employmentSector: string;
+    employmentDuration: string;
+    officeEmail: string;
+    monthlyIncomeRange: UserIncomeRange;
+    loanRepayment: number;
+}
+
+interface UserIncomeRange {
+    lowRange: number;
+    highRange: number;
+}
+
+interface UserSocials {
+    twitter: string;
+    facebook: string;
+    instagram: string;
+}
+
+interface UserGuarantor {
+    id: number;
+    guarantorName: string;
+    guarantorPhoneNumber: string;
+    email: string;
+    relationship: string;
+}
+
+interface UserBankDetails {
+    id: number;
+    bankName: string;
+    accountNumber: string;
+    balance: number;
+}
+
+
